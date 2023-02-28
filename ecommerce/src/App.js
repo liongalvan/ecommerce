20e30ffb604ce97937efd49b392de7039ad93e5a
Liongg <leandroegalvan@gmail.com>
2023-02-28 13:44:24 -0300

    instalando craco

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import './styles/index.css';
 
 function App() {
   return (
     <div className="text-blue-600">
-      caca
+      MAkelo
     </div>
   );
 }
