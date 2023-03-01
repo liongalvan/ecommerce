785b040d06757a657e405fc897c509021359923b
Liongg <leandroegalvan@gmail.com>
2023-03-01 18:14:18 -0300

    creando django-core

--- front/src/hocs/Layout.js
+++ front/src/hocs/Layout.js
@@ -1,13 +1,13 @@
 import {ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
-import { Navbar } from '../components/navigations/Navbar'
+import NavbarL from '../components/navigations/Navbar'
 import { Footer } from '../components/navigations/Footer'
 
 const Layaut = (props) =>{ 
     return(
         <div>
-            <Navbar/>
+            <NavbarL/>
             <ToastContainer autoClose={5000}/>
             {props.children}
             <Footer/>
